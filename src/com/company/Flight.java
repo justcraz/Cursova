0332c8b861a5831fb24b2a0effc681fcac0acc4a
Alexandr Grigoraschuk <64975380+justcraz@users.noreply.github.com>
2022-05-26 18:05:58 +0300

    Update Flight.java

--- src/com/company/Flight.java
+++ src/com/company/Flight.java
@@ -2,34 +2,31 @@ package com.company;
 
 public class Flight {
 
-    private int flightNumber;
-    private String origin;
-    private String destination;
-    private String departureTime;
-    private int capacity;
+    private final int flightNumber;
+    private final String origin;
+    private final String destination;
+    private final String departureTime;
     private int numberOfSeatsleft;
-    private double originalPrice;
+    private final double originalPrice;
 
     public Flight(int fn, String o, String ds, String dp, int c, double op) {
-        if (ds.equals(o) == true) {
+        if (ds.equals(o)) {
             throw new IllegalArgumentException("Пункт призначення та походження однакові");
         }
         flightNumber = fn;
+
         origin = o;
         destination = ds;
         departureTime = dp;
-        capacity = c;
         numberOfSeatsleft = c;
         originalPrice = op;
     }
 
-    public boolean bookASeat() {
+    public void bookASeat() {
         if (numberOfSeatsleft > 0) {
             numberOfSeatsleft = numberOfSeatsleft - 1;
-            return true;
 
         }
-        return false;
     }
 
     public double getPrice() {
@@ -56,44 +53,44 @@ public class Flight {
         return numberOfSeatsleft;
     }
 
-    public int getCapacity() {
-        return capacity;
-    }
-
-    public void setPrice(double p) {
-        originalPrice = p;
-    }
-
-    public void setOrigin(String o) {
-        origin = o;
-    }
-
-    public void setDestination(String d) {
-        destination = d;
-    }
-
-    public void setDepartureTime(String d) {
-        departureTime = d;
-    }
-
-    public void setFlightNumber(int fn) {
-        flightNumber = fn;
-    }
-
-    public void setNumberOfSeatsleft(int s) {
-        numberOfSeatsleft = s;
-    }
-
-    public void setCapacity(int c) {
-        capacity = c;
-    }
+//    public int getCapacity() {
+//        return capacity;
+//    }
+//
+//    public void setPrice(double p) {
+//        originalPrice = p;
+//    }
+//
+//    public void setOrigin(String o) {
+//        origin = o;
+//    }
+//
+//    public void setDestination(String d) {
+//        destination = d;
+//    }
+//
+//    public void setDepartureTime(String d) {
+//        departureTime = d;
+//    }
+//
+//    public void setFlightNumber(int fn) {
+//        flightNumber = fn;
+//    }
+//
+//    public void setNumberOfSeatsleft(int s) {
+//        numberOfSeatsleft = s;
+//    }
+//
+//    public void setCapacity(int c) {
+//        capacity = c;
+//    }
 
     @Override
     public String toString() {
-        String one = "Квиток " + flightNumber;
-        String two = "," + origin + " до " + destination;
-        String three = "," + departureTime;
-        String four = ", Оригінальна ціна: " + originalPrice + "$";
+        String one = "Квиток " + "№" + flightNumber;
+        String two = ", Початок рейсу: " + origin + " Прямує до: " + destination;
+        String three = ", Час та дата відправлення: " + departureTime;
+        String four = ", Ціна квитка: " + originalPrice + "$";
         return one + two + three + four;
     }
 
