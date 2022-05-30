009f71fe5996b38ce789bb992375409b40ada281
Alexandr Grigoraschuk <64975380+justcraz@users.noreply.github.com>
2022-05-30 12:35:42 +0300

    Update Member.java

--- src/com/company/Member.java
+++ src/com/company/Member.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class Member extends Passenger {
 
-    private int yearsOfMembership;
+    private final int yearsOfMembership;
 
     public Member(int yom, int a, String n) {
         super(a, n);
@@ -15,12 +15,21 @@ public class Member extends Passenger {
         if (yearsOfMembership > 5) {
             p = p / 2;
             return p;
-        } else if (yearsOfMembership <= 5 && yearsOfMembership > 1) {
+        } else if (yearsOfMembership > 1) {
             p = (p *9)/10;
             return p;
         }
         return p;
+    }
 
+    @Override
+    public String toString() {
+        return String.format("%s, %d, %s, %d", "true", yearsOfMembership, getName(), getAge());
+    }
+
+    @Override
+    public String viewFormat(){
+        return String.format("%s, вік: %d, скільки років учасник: %d", getName(), getAge(), yearsOfMembership);
     }
 
 }
